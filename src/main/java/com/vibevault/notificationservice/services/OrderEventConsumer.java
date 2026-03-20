d71e905573b5a8dbcbf9d0f4a1429f35787dc2c9
Sparsh <sparsh.raj30@gmail.com>
2026-03-20 10:11:24 +0530

    Add Kafka consumers for order and payment events
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

---
+++ src/main/java/com/vibevault/notificationservice/services/OrderEventConsumer.java
@@ -0,0 +1,35 @@
+package com.vibevault.notificationservice.services;
+
+import com.vibevault.notificationservice.constants.KafkaTopics;
+import com.vibevault.notificationservice.events.OrderEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class OrderEventConsumer {
+
+    private final NotificationDispatcher notificationDispatcher;
+
+    @KafkaListener(
+            topics = KafkaTopics.ORDER_EVENTS,
+            groupId = "notificationservice",
+            containerFactory = "orderEventListenerContainerFactory"
+    )
+    public void handleOrderEvent(OrderEvent event) {
+        log.info("Received order event: {} for order {}", event.getEventType(), event.getOrderId());
+
+        switch (event.getEventType()) {
+            case "ORDER_CREATED" -> notificationDispatcher.notifyOrderCreated(
+                    event.getUserId(), event.getOrderId(), event.getTotalAmount(), event.getCurrency());
+            case "ORDER_CONFIRMED" -> notificationDispatcher.notifyOrderConfirmed(
+                    event.getUserId(), event.getOrderId());
+            case "ORDER_CANCELLED" -> notificationDispatcher.notifyOrderCancelled(
+                    event.getUserId(), event.getOrderId());
+            default -> log.debug("Ignoring order event type: {}", event.getEventType());
+        }
+    }
+}
---
+++ src/main/java/com/vibevault/notificationservice/services/PaymentEventConsumer.java
@@ -0,0 +1,33 @@
+package com.vibevault.notificationservice.services;
+
+import com.vibevault.notificationservice.constants.KafkaTopics;
+import com.vibevault.notificationservice.events.PaymentEvent;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PaymentEventConsumer {
+
+    private final NotificationDispatcher notificationDispatcher;
+
+    @KafkaListener(
+            topics = KafkaTopics.PAYMENT_EVENTS,
+            groupId = "notificationservice",
+            containerFactory = "paymentEventListenerContainerFactory"
+    )
+    public void handlePaymentEvent(PaymentEvent event) {
+        log.info("Received payment event: {} for order {}", event.getEventType(), event.getOrderId());
+
+        switch (event.getEventType()) {
+            case "PAYMENT_CONFIRMED" -> notificationDispatcher.notifyPaymentConfirmed(
+                    event.getUserId(), event.getOrderId(), event.getAmount());
+            case "PAYMENT_FAILED" -> notificationDispatcher.notifyPaymentFailed(
+                    event.getUserId(), event.getOrderId(), event.getFailureReason());
+            default -> log.debug("Ignoring payment event type: {}", event.getEventType());
+        }
+    }
+}
