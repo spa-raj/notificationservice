db7330b38a9b5c4c51181fb6d93f939dfb264005
Sparsh <sparsh.raj30@gmail.com>
2026-03-20 10:10:40 +0530

    Add KafkaTopics constants
    Signed-off-by: Sparsh <sparsh.raj30@gmail.com>

---
+++ src/main/java/com/vibevault/notificationservice/constants/KafkaTopics.java
@@ -0,0 +1,9 @@
+package com.vibevault.notificationservice.constants;
+
+public final class KafkaTopics {
+    public static final String ORDER_EVENTS = "order-events";
+    public static final String PAYMENT_EVENTS = "payment-events";
+
+    private KafkaTopics() {
+    }
+}
